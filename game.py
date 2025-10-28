82ec086ee83c3899f82b43394f4cbb4496b60172
DavidPopovich3261 <8534397@gmail.com>
2025-10-28 09:53:36 +0200

    first commit

---
+++ game.py
@@ -0,0 +1,36 @@
+
+# def Creat_plger():
+#     Steps=
+
+def Creating_a_card_deck(size:int)->list[list]:
+    pass
+
+
+
+def parallel_matrix(size:int)->list[list]:
+    pass
+
+
+def Checking_for_proper_input(cards):
+    pass
+
+
+def step_move():
+    pass
+
+
+
+
+def mixing(deck:list[list])->list[list]:
+    pass
+
+
+
+def is_won():
+    pass
+
+
+def is_los():
+    pass
+
+
---
+++ io.py
@@ -0,0 +1,13 @@
+
+
+
+def input_size()->int:
+    pass
+
+
+def input_cards():
+    pass
+
+
+def print_metrix()->None:
+    pass
